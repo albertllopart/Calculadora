b91c22afb4e7feed858a0c2a4a940ecc41ae947f
albertllopart <d.pocafeiner@gmail.com>
2018-09-22 00:42:56 +0200

    feina a casa
    acabar calculadora amb operacions múltiples, clear btn, etc.

--- app/src/main/java/com/example/albertln/calculadora/CalculatorActivity.java
+++ app/src/main/java/com/example/albertln/calculadora/CalculatorActivity.java
@@ -16,7 +16,9 @@ public class CalculatorActivity extends AppCompatActivity {
     private double operator_2 = 0;
     private boolean operator_2_check = false;
 
-    private int sign = 292; //0=suma, 1=resta, 2=mult, 3=div
+    private boolean multi_operation = false;
+
+    private int sign = 0; //0=suma, 1=resta, 2=mult, 3=div
 
     private double result = 0;
 
@@ -31,6 +33,12 @@ public class CalculatorActivity extends AppCompatActivity {
 
     public void onClickDigit(View view){
 
+        if (multi_operation == true)
+        {
+            number = "";
+            number_view.setText(number);
+            multi_operation = false;
+        }
         Button b = (Button)view;
         number += b.getText().toString().charAt(0);
         number_view.setText(number);
@@ -47,42 +55,98 @@ public class CalculatorActivity extends AppCompatActivity {
     public void onClickOperator(View view){
 
         Button b = (Button)view;
-        String symbol = b.getText().toString();
 
         if (operator_1_check == false){
             operator_1_check = true;
         }
 
-        number = "";
-        number_view.setText(number);
+        if (operator_2_check == true)
+        {
+            if (sign == 0)
+            {
+                result = operator_1 + operator_2;
+            }
+            else if (sign == 1)
+            {
+                result = operator_1 - operator_2;
+            }
+            else if (sign == 2)
+            {
+                result = operator_1 * operator_2;
+            }
+            else if (sign == 3)
+            {
+                result = operator_1 / operator_2;
+            }
 
-        if (symbol == "+"){
+            operator_1 = result;
+            operator_2_check = false;
+            number = Double.toString(result);
+            number_view.setText(number);
+            multi_operation = true;
+        }
+
+        if (b.getId() == R.id.btn_plus){
             sign = 0;
         }
-        else if (symbol == "-"){
+        else if (b.getId() == R.id.btn_minus){
             sign = 1;
         }
-        else if (symbol == "*"){
+        else if (b.getId() == R.id.btn_product){
             sign = 2;
         }
-        else if (symbol == "/"){
+        else if (b.getId() == R.id.btn_division){
             sign = 3;
         }
+
+        number = "";
+        number_view.setText(number);
     }
 
     public void onClickDot(View view){
 
-
+        Button b = (Button)view;
+        number += b.getText().toString().charAt(0);
+        number_view.setText(number);
     }
 
     public void onClickEquals(View view){
 
         if (operator_2_check = true)
         {
-            result = operator_1 + operator_2;
+            if (sign == 0)
+            {
+                result = operator_1 + operator_2;
+            }
+            else if (sign == 1)
+            {
+                result = operator_1 - operator_2;
+            }
+            else if (sign == 2)
+            {
+                result = operator_1 * operator_2;
+            }
+            else if (sign == 3)
+            {
+                result = operator_1 / operator_2;
+            }
+
+            operator_1 = result;
+            operator_2_check = false;
             number = Double.toString(result);
             number_view.setText(number);
         }
+    }
+
+    public void onClickClear(View view){
+
+        operator_1 = 0;
+        operator_1_check = false;
+        operator_2 = 0;
+        operator_2_check = false;
+
+        number = "";
+        number_view.setText(number);
     }
 
 }
